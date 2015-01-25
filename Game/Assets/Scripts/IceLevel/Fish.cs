b6f5f0f0863853346213fb4ca3e24b8bbefbe4ec
Emily Bird <applelasting@gmail.com>
2015-01-25 09:23:55 -0800

    Added eating fish mini-game scene.

---
+++ Game/Assets/Scripts/IceLevel/Fish.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fish : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+
+	}
+
+    void FixedUpdate()
+    {
+        GameObject penguin = GameObject.Find("penguin");
+        Vector3 fishPosition = transform.position;
+        Vector3 penguinPosition = penguin.rigidbody2D.position;
+        float distanceBetweenFishAndPenguin = Mathf.Sqrt(Mathf.Pow((penguinPosition.x - fishPosition.x), 2) + Mathf.Pow((penguinPosition.y - fishPosition.y), 2));
+
+        if (distanceBetweenFishAndPenguin < 10)
+        {
+            // penguin is too close, move away from penguin
+            Vector2 penguinVelocity = penguin.rigidbody2D.velocity;
+            rigidbody2D.AddForce(20*penguinVelocity);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "penguin")
+        {
+            // update score
+            FishScore.updateScore();
+            Destroy(this.gameObject);
+        }
+    }
+}

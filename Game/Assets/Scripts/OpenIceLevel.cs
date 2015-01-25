ee2bbad5df522f41cba756661bb516b7df27c283
jennylia <jennylian100@gmail.com>
2015-01-24 16:20:14 -0800

    Committing Everything, but basically a menu is done

---
+++ Game/Assets/Scripts/OpenIceLevel.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenIceLevel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.tag == "Player") 
+		{
+			Debug.Log ("Player has come");
+
+			if(this.tag == "IceLevel"){
+				Debug.Log ("IceLevelInitiated");
+				Application.LoadLevel("JennyScene");
+			}
+
+			if(this.tag == "BoxLevel"){
+				Debug.Log ("BoxLevel");
+				Application.LoadLevel("JennyScene");
+
+			}
+
+			if(this.tag == "MountainLevel"){
+				Debug.Log ("MountainLevel");
+				Application.LoadLevel("JennyScene");
+
+			}
+
+			if(this.tag == "DoorLevel"){
+				Debug.Log ("DoorLevel");
+				Application.LoadLevel("JennyScene");
+
+			}
+			//Destroy (this.gameObject, (float) 0.25);
+			
+		}
+		MainCharacter.score += 10000;
+		
+	}
+}
---
+++ Game/Assets/Scripts/menuPlayerMovement.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class menuPlayerMovement : MonoBehaviour {
+	
+	public static int score;
+	//public static string name = "Jo";
+	
+	// Use this for initialization
+	void Start () {
+		score = 10;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Debug.Log (score);
+		//score ++;
+	}
+	
+	void FixedUpdate()
+	{
+		float positionX = transform.position.x;
+		float positionY = transform.position.y;
+		float movementSpeed = (float)0.4;
+		//float boostSpeed = (float)1.5;
+		
+		// Move right
+		if (Input.GetKey("d"))
+		{
+			
+			transform.position = new Vector3(positionX + movementSpeed, positionY, 0);
+			positionX = transform.position.x;
+		}
+		
+		// Move left
+		if (Input.GetKey("a"))
+		{
+			transform.position = new Vector3(positionX - movementSpeed, positionY, 0);
+			positionX = transform.position.x;
+		}
+		
+		// Move up
+		if (Input.GetKey("w"))
+		{
+			transform.position = new Vector3(positionX, positionY + movementSpeed, 0);
+			positionY = transform.position.y;
+			//			if(Input.GetKey("w")){
+			//				transform.position = new Vector3(positionX, positionY + boostSpeed, 0);
+			//				positionY = transform.position.y;
+			//
+			//			}
+		}
+		
+		// Move down
+		if (Input.GetKey("s"))
+		{
+			transform.position = new Vector3(positionX, positionY - movementSpeed, 0);
+			positionY = transform.position.y;
+		}
+	}
+}
